90c88725f4f41505fc71e4a419dc5a73e72ca002
DianaHang <dianavegat99@gmail.com>
2025-11-11 22:46:38 -0600

    Update src directory and backend main class

---
+++ src/com/enciclopedia/backend/Usuario.java
@@ -0,0 +1,12 @@
+package com.enciclopedia.backend;
+
+import java.util.ArrayList;
+
+public class Usuario {
+	//Atributos
+	protected String nombreUsuario;
+	protected String password;
+	protected String fotoUsuario;
+	private ArrayList<Civilizacion> civFav;
+	
+}
