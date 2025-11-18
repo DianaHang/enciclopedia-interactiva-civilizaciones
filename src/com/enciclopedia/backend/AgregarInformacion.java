5bcae794139787669c3bbf3da4be7d798f65f53e
DianaHang <dianavegat99@gmail.com>
2025-11-17 21:40:13 -0600

    Create AddInfo Interface

---
+++ src/com/enciclopedia/backend/AgregarInformacion.java
@@ -0,0 +1,8 @@
+package com.enciclopedia.backend;
+
+public interface AgregarInformacion {
+	//Métodos (abstractos)
+	public void agregarPersonaje(Personaje p);
+	
+	public void agregarEvento(Evento e);
+}
