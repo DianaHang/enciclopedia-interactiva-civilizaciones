f9fa503b0a723705b030f7a63b0439bf9e8dca51
Marco Antonio Jacobo <mjacobo@MacBook-Air-de-MC.local>
2025-11-18 09:07:36 -0600

    Se agregan metodos en la clase Adminsitrador

--- src/com/enciclopedia/backend/Administrador.java
+++ src/com/enciclopedia/backend/Administrador.java
@@ -7,7 +7,7 @@ public class Administrador extends Usuario implements AgregarInformacion {
 	 private GestorEnciclopedia gestor;
 	
 	//Constructor (de herencia)
-	public Administrador(String nombreUsuario, String password, String fotoUsuario, ArrayList<Civilizacion> civFav) {
+	public Administrador(String nombreUsuario, String password, String fotoUsuario, ArrayList<Civilizacion> civFav,GestorEnciclopedia gestor) {
 		super(nombreUsuario, password, fotoUsuario, civFav);
 		this.gestor = gestor;
 	}
@@ -21,7 +21,7 @@ public class Administrador extends Usuario implements AgregarInformacion {
 		this.gestor = gestor;
 	}
 
-	//Métodos
+	//Métodos de administrador para civilizaciones 
 	 public void agregarCivilizacion(Civilizacion c) {
 	        gestor.agregarCivilizacion(c);
 	    }
@@ -34,34 +34,53 @@ public class Administrador extends Usuario implements AgregarInformacion {
 	    public void eliminarCivilizacion(Civilizacion c) {
 	        gestor.eliminarCivilizacion(c);
 	    }
+
+		//Metodos para personajes 
 	
 	@Override
 	public void agregarPersonaje(Personaje p) {
 		// TODO Auto-generated method stub
 		
 	}
-	
-	public void editarPersonaje() {
-			
-	}
-		
-	public void eliminarPersonaje() {
-			
-	}
 
+	public void agregarPersonaje(Personaje p, Civilizacion civ) {
+        if (civ != null) {
+            civ.agregarPersonaje(p);
+        }
+    }
+	
+    public void editarPersonaje(Personaje p, String nuevoNombre, String nuevosAportes, String nuevaFoto) {
+        p.setNombre(nuevoNombre);
+        p.setAportes(nuevosAportes);
+        p.setFotoPers(nuevaFoto);
+    }
+
+    public void eliminarPersonaje(Personaje p, Civilizacion civ) {
+        if (civ != null && civ.getPersonaje() != null) {
+            civ.getPersonaje().remove(p);
+        }
+    }
+// Métodods para eventos 
 	@Override
 	public void agregarEvento(Evento e) {
 		// TODO Auto-generated method stub
 		
 	}
 	
-	public void editarEvento() {
-		
-	}
-		
-	public void eliminarEvento() {
-			
-	}
-	
+	public void agregarEvento(Evento e, Civilizacion civ) {
+        if (civ != null) {
+            civ.agregarEvento(e);
+        }
+    }
 
+    public void editarEvento(Evento e, String nuevoTitulo, String nuevaDesc) {
+        e.setTitulo(nuevoTitulo);
+        e.setDescripcion(nuevaDesc);
+    }
+
+    public void eliminarEvento(Evento e, Civilizacion civ) {
+        if (civ != null && civ.getEvento() != null) {
+            civ.getEvento().remove(e);
+        }
+	}
 }
